3c9721b0096b5503585f8525f588662c8cba41b4
DeanDunbar <the.dean.dunbar@gmail.com>
2014-12-11 00:03:31 -0600

    removed morris plugin because errors

--- application/views/theme.php
+++ application/views/theme.php
@@ -372,9 +372,7 @@
     <script src="<?php echo base_url(); ?>matrix/themes/sbadmin/js/jquery.js"></script>
     <script src="<?php echo base_url(); ?>matrix/themes/sbadmin/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url(); ?>matrix/themes/sbadmin/js/plugins/metisMenu/metisMenu.min.js"></script>
-    <script src="<?php echo base_url(); ?>matrix/themes/sbadmin/js/plugins/morris/raphael.min.js"></script>
-    <script src="<?php echo base_url(); ?>matrix/themes/sbadmin/js/plugins/morris/morris.min.js"></script>
-    <script src="<?php echo base_url(); ?>matrix/themes/sbadmin/js/plugins/morris/morris-data.js"></script>
+
     <script src="<?php echo base_url(); ?>matrix/plugins/dynatable/jquery.dynatable.js"></script>
     <script src="<?php echo base_url(); ?>matrix/themes/sbadmin/js/sb-admin-2.js"></script>
 
