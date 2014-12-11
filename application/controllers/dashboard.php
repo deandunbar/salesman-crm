0af72f3f41ca0c3b62f4bc726f3145c0bc123487
DeanDunbar <the.dean.dunbar@gmail.com>
2014-12-10 19:34:45 -0600

    theme

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		//~ $this->load->view('welcome_message');
+		
+		
+		
+		//~ $id = 1;
+		//~ $location_name = "Waco, TX";
+		//~ 
+		//~ $restaraunts_list = $this->restaraunt_modal->get_restaraunts_by_location($id);
+		$data['temp'] = "temp";
+		
+		$main_content = $this->load->view('widgets/dashboard', $data, true); // Select our view file that will display our main area
+		//~ $restaraunt_list_main_content = $this->load->view('widgets/restaraunt_list_main_content', $data, true);
+		
+		//~ echo $content;
+		
+		$data['main_content'] = $main_content;
+		
+		$this->load->view('theme.php', $data);
+		
+		
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
