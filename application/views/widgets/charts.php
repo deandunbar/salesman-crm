aeb32bbd55bc80ac6128fd78f120f7cc5c7b8401
Harris Dagha <harris_dagha@baylor.edu>
2014-12-11 22:00:16 -0600

    chartjs

--- application/views/widgets/charts.php
+++ application/views/widgets/charts.php
@@ -11,7 +11,11 @@
                         <!-- /.panel-heading -->
                         <div class="panel-body medium_minimum_height">
                             <div class="well">
-                                <canvas id="myChart" width="100%" height="100%">
+                                <canvas id="myChart" width="800" height="500" style="width: 100% !important;max-width: 800px;height: auto !important;">
+
+                            </div>
+                            <div class="well">
+                                <canvas id="myChart2" width="800" height="500" style="width: 100% !important;max-width: 800px;height: auto !important;">
                             </div>
                         </div>
                         <!-- /.panel-body -->
@@ -25,8 +29,12 @@
 
 
 $(function() {
-                var ctx = $("#myChart").get(0).getContext("2d");
 
+        //declare
+                var ctx = $("#myChart").get(0).getContext("2d");
+                var ctx2 = $("#myChart2").get(0).getContext("2d");
+
+        //line data
                 var data = {
                     labels: ["January", "February", "March", "April", "May", "June", "July"],
                     datasets: [
@@ -47,7 +55,34 @@ $(function() {
                     ]
                 }
 
+        //bar data
+                var data = {
+                    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+                    datasets: [
+                        {
+                            label: "My First dataset",
+                            fillColor: "rgba(220,220,220,0.2)",
+                            strokeColor: "rgba(220,220,220,1)",
+                            pointColor: "rgba(220,220,220,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: [65, 59, 90, 81, 56, 55, 40]
+                        },
+                        {
+                            label: "My Second dataset",
+                            fillColor: "rgba(151,187,205,0.2)",
+                            strokeColor: "rgba(151,187,205,1)",
+                            pointColor: "rgba(151,187,205,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(151,187,205,1)",
+                            data: [28, 48, 40, 19, 96, 27, 100]
+                        }
+                    ]
+                };
                 var myNewChart = new Chart(ctx).Line(data);
+                var myRadarChart = new Chart(ctx2).Radar(data);
             });
 </script>
 
