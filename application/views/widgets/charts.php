f4cb14a12a748681b22952adf2318f3b2052cad5
Harris Dagha <harris_dagha@baylor.edu>
2014-12-11 23:02:26 -0600

    chartjs colors

--- application/views/widgets/charts.php
+++ application/views/widgets/charts.php
@@ -69,9 +69,9 @@ $(function() {
                     datasets: [
                         {
                             label: "My First dataset",
-                            fillColor: "rgba(220,220,220,0.2)",
-                            strokeColor: "rgba(220,220,220,1)",
-                            pointColor: "rgba(220,220,220,1)",
+                            fillColor: "rgba(220,220,1,0.2)",
+                            strokeColor: "rgba(220,220,1,1)",
+                            pointColor: "rgba(220,220,1,1)",
                             pointStrokeColor: "#fff",
                             pointHighlightFill: "#fff",
                             pointHighlightStroke: "rgba(220,220,220,1)",
