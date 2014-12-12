3ef53724cc7a7a6ac4c9e45caf3716f263a52a38
DeanDunbar <the.dean.dunbar@gmail.com>
2014-12-11 23:14:29 -0600

    Search of users now works. CELEBRATE NOW

--- application/models/contacts_modal.php
+++ application/models/contacts_modal.php
@@ -41,6 +41,53 @@ Class Contacts_modal extends CI_Model
 	}
 	
 	
+	/*
+	 * gets the list of all the contacts wanted
+	 * RETURNS: ARRAY OF contacts
+	 * 
+	 * Parameters: 	
+	 * $lim (optional) = limit for the contacts to return. Defaults to 10
+	 * $off (optional) = the offset for the results that are returned
+	 * 
+	 *
+	 */
+	function search_contacts($lim = NULL, $off = NULL, $q = NULL){
+		if (!isset($lim)) {
+	    $lim = 10;
+		}
+		if (!isset($off)) {
+		    $off = 0;
+		}
+		if (!isset($q)) {
+		    $q = "";
+		}
+		$match = $q;
+		
+		$this -> db -> select('Contact_ID, Company_ID,First_Name, Last_Name, Phone, Job_Title, Contact_Date');
+		$this -> db -> from('Contact');
+		//~ $this -> db -> where('Location_ID', $location_id);
+		$this->db->like('First_Name', $match);
+		$this->db->or_like('Last_Name', $match); 
+		$this -> db -> limit($lim, $off);
+		
+		
+		$query = $this -> db -> get();
+	
+		
+		
+		  if($query -> num_rows() > 0){
+			$results["data"] = $query->result();
+			$results["num_rows"] = $query -> num_rows();
+			return $results;
+		   }
+		   else
+		   {
+		     return false;
+		   }
+		
+	}
+	
+	
 	
 	
 	
