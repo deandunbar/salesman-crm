5f51249ad65731258c9a57a1f8d5e300b33fbee5
Harris Dagha <harris_dagha@baylor.edu>
2014-12-11 22:04:27 -0600

    chartjs bar done

--- application/views/widgets/charts.php
+++ application/views/widgets/charts.php
@@ -17,6 +17,9 @@
                             <div class="well">
                                 <canvas id="myChart2" width="800" height="500" style="width: 100% !important;max-width: 800px;height: auto !important;">
                             </div>
+                            <div class="well">
+                                <canvas id="myChart3" width="800" height="500" style="width: 100% !important;max-width: 800px;height: auto !important;">
+                            </div>
                         </div>
                         <!-- /.panel-body -->
                     </div>
@@ -33,6 +36,8 @@ $(function() {
         //declare
                 var ctx = $("#myChart").get(0).getContext("2d");
                 var ctx2 = $("#myChart2").get(0).getContext("2d");
+                var ctx3 = $("#myChart3").get(0).getContext("2d");
+
 
         //line data
                 var data = {
@@ -56,7 +61,7 @@ $(function() {
                 }
 
         //bar data
-                var data = {
+                var data2 = {
                     labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
                     datasets: [
                         {
@@ -81,8 +86,32 @@ $(function() {
                         }
                     ]
                 };
+
+        // bar data
+                var data3 = {
+                    labels: ["January", "February", "March", "April", "May", "June", "July"],
+                    datasets: [
+                        {
+                            label: "My First dataset",
+                            fillColor: "rgba(220,220,220,0.5)",
+                            strokeColor: "rgba(220,220,220,0.8)",
+                            highlightFill: "rgba(220,220,220,0.75)",
+                            highlightStroke: "rgba(220,220,220,1)",
+                            data: [65, 59, 80, 81, 56, 55, 40]
+                        },
+                        {
+                            label: "My Second dataset",
+                            fillColor: "rgba(151,187,205,0.5)",
+                            strokeColor: "rgba(151,187,205,0.8)",
+                            highlightFill: "rgba(151,187,205,0.75)",
+                            highlightStroke: "rgba(151,187,205,1)",
+                            data: [28, 48, 40, 19, 86, 27, 90]
+                        }
+                    ]
+                };
                 var myNewChart = new Chart(ctx).Line(data);
-                var myRadarChart = new Chart(ctx2).Radar(data);
+                var myRadarChart = new Chart(ctx2).Radar(data2);
+                var myBarChart = new Chart(ctx3).Bar(data3);
             });
 </script>
 
