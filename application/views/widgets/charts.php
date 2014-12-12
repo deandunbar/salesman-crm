469dc60d271683fa26c03c895b6b236228a1efeb
Harris Dagha <harris_dagha@baylor.edu>
2014-12-11 20:55:35 -0600

    chartjs

--- application/views/widgets/charts.php
+++ application/views/widgets/charts.php
@@ -11,7 +11,7 @@
                         <!-- /.panel-heading -->
                         <div class="panel-body medium_minimum_height">
                             <div class="well">
-
+                                <canvas id="myChart" width="400" height="400"></canvas>
                             </div>
                         </div>
                         <!-- /.panel-body -->
@@ -22,5 +22,38 @@
 </div>
             <!-- /.row -->
 
+            <script>
+            var ctx = document.getElementById("myChart").getContext("2d");
+            var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+        ]
+    };
+
+new Chart(ctx).PolarArea(data, options);
+</script>
+
+
 <script src="<?php echo base_url(); ?>matrix/plugins/chartjs/Chart.js"></script>
 <script src="<?php echo base_url(); ?>matrix/plugins/chartjs/Custom.js"></script>
+
