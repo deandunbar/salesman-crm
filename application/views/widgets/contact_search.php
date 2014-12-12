3d510c1ff27341f6bd1f44f5c161c018f896fb69
DeanDunbar <the.dean.dunbar@gmail.com>
2014-12-11 19:31:17 -0600

    added css classes to the contact search table holder

--- application/views/widgets/contact_search.php
+++ application/views/widgets/contact_search.php
@@ -1,14 +1,14 @@
 <?php
 
 ?>
-<div class="row">
+<div class="row raise_the_roof">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             DataTables Advanced Tables
                         </div>
                         <!-- /.panel-heading -->
-                        <div class="panel-body">
+                        <div class="panel-body medium_minimum_height">
                             <div class="well">
                             </div>
                         </div>
