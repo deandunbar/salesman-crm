c5a4e1c2b8f79f30aa44965dbc44f916a786bb51
Harris Dagha <harris_dagha@baylor.edu>
2014-12-11 21:39:40 -0600

    chartjs

--- application/views/widgets/charts.php
+++ application/views/widgets/charts.php
@@ -1,13 +1,32 @@
 <?php
 
 ?>
- <script type="text/javascript">
 
-            window.onLoad = function() {
-                init();
-            };
+<div class="row raise_the_roof">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            DataTables Advanced Tables
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body medium_minimum_height">
+                            <div class="well">
+                                <canvas id="myChart" width="400" height="400">
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+</div>
+            <!-- /.row -->
+<script>
+            window.onload = graph;
 
-            function init() {
+
+            function graph() {
+                alert("Hello! I am an alert box!!");
                 var ctx = $("#myChart").get(0).getContext("2d");
 
                 var data = {
@@ -33,63 +52,6 @@
                 var myNewChart = new Chart(ctx).Line(data);
             }
 
-        </script>
 
-<div class="row raise_the_roof">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            DataTables Advanced Tables
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body medium_minimum_height">
-                            <div class="well">
-                                <canvas id="myChart" width="400" height="400">
-                            </div>
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-</div>
-            <!-- /.row -->
-
-            <script>
-//             var ctx = document.getElementById("myChart").getContext("2d");
-//            // var myNewChart = new Chart(ctx).PolarArea(data);
-//             var myLineChart = new Chart(ctx).Line(data, options);
-//             var data = {
-//     labels: ["January", "February", "March", "April", "May", "June", "July"],
-//     datasets: [
-//         {
-//             label: "My First dataset",
-//             fillColor: "rgba(220,220,220,0.2)",
-//             strokeColor: "rgba(220,220,220,1)",
-//             pointColor: "rgba(220,220,220,1)",
-//             pointStrokeColor: "#fff",
-//             pointHighlightFill: "#fff",
-//             pointHighlightStroke: "rgba(220,220,220,1)",
-//             data: [65, 59, 80, 81, 56, 55, 40]
-//         },
-//         {
-//             label: "My Second dataset",
-//             fillColor: "rgba(151,187,205,0.2)",
-//             strokeColor: "rgba(151,187,205,1)",
-//             pointColor: "rgba(151,187,205,1)",
-//             pointStrokeColor: "#fff",
-//             pointHighlightFill: "#fff",
-//             pointHighlightStroke: "rgba(151,187,205,1)",
-//             data: [28, 48, 40, 19, 86, 27, 90]
-//         }
-//         ]
-//     };
-// var myLineChart = new Chart(ctx);
-// new Chart(ctx).Line(data, options);
 </script>
-
-
-
-
-
 
