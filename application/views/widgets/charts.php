1a014d3b215fcddf18b764001691bd2700f28025
Harris Dagha <harris_dagha@baylor.edu>
2014-12-11 21:17:02 -0600

    chartjs

--- application/views/widgets/charts.php
+++ application/views/widgets/charts.php
@@ -1,6 +1,39 @@
 <?php
 
 ?>
+ <script type="text/javascript">
+
+            window.onLoad = function() {
+                init();
+            };
+
+            function init() {
+                var ctx = $("#myChart").get(0).getContext("2d");
+
+                var data = {
+                    labels: ["January", "February", "March", "April", "May", "June", "July"],
+                    datasets: [
+                        {
+                            fillColor: "rgba(220,220,220,0.5)",
+                            strokeColor: "rgba(220,220,220,1)",
+                            pointColor: "rgba(220,220,220,1)",
+                            pointStrokeColor: "#fff",
+                            data: [65, 59, 90, 81, 56, 55, 40]
+                        },
+                        {
+                            fillColor: "rgba(151,187,205,0.5)",
+                            strokeColor: "rgba(151,187,205,1)",
+                            pointColor: "rgba(151,187,205,1)",
+                            pointStrokeColor: "#fff",
+                            data: [28, 48, 40, 19, 96, 27, 100]
+                        }
+                    ]
+                }
+
+                var myNewChart = new Chart(ctx).Line(data);
+            }
+
+        </script>
 
 <div class="row raise_the_roof">
                 <div class="col-lg-12">
@@ -11,7 +44,7 @@
                         <!-- /.panel-heading -->
                         <div class="panel-body medium_minimum_height">
                             <div class="well">
-                                <canvas id="myChart" width="400" height="400"></canvas>
+                                <canvas id="myChart" width="400" height="400">
                             </div>
                         </div>
                         <!-- /.panel-body -->
@@ -23,37 +56,39 @@
             <!-- /.row -->
 
             <script>
-            var ctx = document.getElementById("myChart").getContext("2d");
-           // var myNewChart = new Chart(ctx).PolarArea(data);
-            var myLineChart = new Chart(ctx).Line(data, options);
-            var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-        ]
-    };
-var myLineChart = new Chart(ctx);
-new Chart(ctx).Line(data, options);
+//             var ctx = document.getElementById("myChart").getContext("2d");
+//            // var myNewChart = new Chart(ctx).PolarArea(data);
+//             var myLineChart = new Chart(ctx).Line(data, options);
+//             var data = {
+//     labels: ["January", "February", "March", "April", "May", "June", "July"],
+//     datasets: [
+//         {
+//             label: "My First dataset",
+//             fillColor: "rgba(220,220,220,0.2)",
+//             strokeColor: "rgba(220,220,220,1)",
+//             pointColor: "rgba(220,220,220,1)",
+//             pointStrokeColor: "#fff",
+//             pointHighlightFill: "#fff",
+//             pointHighlightStroke: "rgba(220,220,220,1)",
+//             data: [65, 59, 80, 81, 56, 55, 40]
+//         },
+//         {
+//             label: "My Second dataset",
+//             fillColor: "rgba(151,187,205,0.2)",
+//             strokeColor: "rgba(151,187,205,1)",
+//             pointColor: "rgba(151,187,205,1)",
+//             pointStrokeColor: "#fff",
+//             pointHighlightFill: "#fff",
+//             pointHighlightStroke: "rgba(151,187,205,1)",
+//             data: [28, 48, 40, 19, 86, 27, 90]
+//         }
+//         ]
+//     };
+// var myLineChart = new Chart(ctx);
+// new Chart(ctx).Line(data, options);
 </script>
+
+
 
 
 
